ed460b7ae72c4e01d36d35a39fb39ba30ca2a4a5
Prabhdeep Kainth <pdkainth@gmail.com>
2021-01-17 14:22:24 -0800

    merged text recognition and nutrition info

--- app/src/main/java/com/example/health360_v2/MainCamera.java
+++ app/src/main/java/com/example/health360_v2/MainCamera.java
@@ -1,14 +1,52 @@
 package com.example.health360_v2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainCamera extends AppCompatActivity {
+import com.example.health360_tools.TextNutrition;
+
+public class MainCamera extends AppCompatActivity implements ImageToText.ImageToTextCompleteListener {
+
+    private ImageToText imageToText;
+    private static final String TAG = "CAMERA_ACTIVITY";
+    private Button camButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_camera_activity);
+
+        camButton = findViewById(R.id.cam_button_2);
+        camButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Starting image to text");
+                imageToText.start();
+            }
+        });
+
+        imageToText = new ImageToText(getApplicationContext(), this);
+        imageToText.setOnImageToTextComplete(this);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        imageToText.extractText(requestCode, resultCode, data);
+
+    }
+
+    @Override
+    public void onImageToTextComplete(String s) {
+        //Log.d(TAG, s);
+        Log.d(TAG, new TextNutrition(s.split("\n")).toString());
     }
 }
